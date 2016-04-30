81da1f285fc5e1712980fcd9b5b0d8c5c476c1d4
tyger <tyger@163.com>
2016-04-30 20:34:11 +0800

    finish sample3-1.

--- Sampe3-1/src/com/example/opengl/MainActivity.java
+++ Sampe3-1/src/com/example/opengl/MainActivity.java
@@ -5,7 +5,7 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class MainActivity extends Activity{
-
+	MyTDView mview;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -13,19 +13,24 @@ public class MainActivity extends Activity{
         //设置为竖屏模式
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		
-		
+        mview=new MyTDView(this);
+        mview.requestFocus();
+        mview.setFocusableInTouchMode(true);
+        setContentView(mview);
 	}
 
 	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
+		mview.onPause();
 	}
 
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
+		mview.onResume();
 	}
 
 }
